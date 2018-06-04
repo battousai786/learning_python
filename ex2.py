1108a2bf6a9d7caa4a05484287c3146cc309aae3
Syed Muhammad Nayyar Mustafa <nayyar.mustafa.bokhari@gmail.com>
2018-06-04 06:51:30 -0400

    Second Commit

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#A cooment, this is so you can read you program later
+#Anything after the # is ignored by Python
+
+print("I could have code like this.") #And the comment after this is ignored
+
+#You can also use comment to "disable" or comment out code:
+#print("This won't run.")
+
+print("This will run.")
