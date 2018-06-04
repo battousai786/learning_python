5c4ef8cf2caf682b8ac263f54c322c425f6260e2
Syed Muhammad Nayyar Mustafa <nayyar.mustafa.bokhari@gmail.com>
2018-06-04 08:44:52 -0400

    Third Commit

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Syed Muhammad Nayyar Mustafa'
+my_age = 32
+my_height = 65
+my_weight = 143
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall which is {my_height * 2.54} centimeters.")
+print(f"He's {my_weight} pounds heavy which equates to about {round(my_weight / 2.21, 2)} kilograms.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height} and {my_weight} I get {total}.")
---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = 'Mon Tue Wed Thu Fri Sat Sun'
+months = 'Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec'
+
+print("Here are the days: ",days)
+print("Here are the momths: ",months)
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want. Or 5. Or 6.
+""")
